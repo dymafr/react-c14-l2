2795cdcf03e764a634fc75f72a889e80b97b8543
dymafr <erwan1309@hotmail.com>
2022-07-07 11:50:30 +0200

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 function App() {
+  const { register, handleSubmit } = useForm();
+
+  function submit(values) {
+    console.log(values);
+  }
+
   return (
     <div
       className="d-flex justify-content-center align-items-center"
       style={{ backgroundColor: '#fefefe', height: '100vh', width: '100%' }}
     >
-      <form>
+      <form onSubmit={handleSubmit(submit)}>
         <div className="d-flex flex-column mb-20">
           <label htmlFor="name" className="mb-5">
             Nom
           </label>
-          <input id="name" type="text" />
+          <input id="name" type="text" {...register('name')} />
         </div>
         <button className="btn btn-primary">Sauvegarder</button>
       </form>
